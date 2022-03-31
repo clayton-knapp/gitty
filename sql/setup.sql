fe62d5806c410bc3fa4bea42193cca19fd29b296
Clayton Knapp <csjknapp@gmail.com>
2022-03-30 17:02:05 -0700

    changed sql table, added static server frontend

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,24 +1,25 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 
-DROP TABLE IF EXISTS github_users;
+DROP TABLE IF EXISTS github_users CASCADE;
+DROP TABLE IF EXISTS tweets CASCADE;
 
 CREATE TABLE github_users (
-  username TEXT NOT NULL,
-  email TEXT NOT NULL,
+  email TEXT NOT NULL PRIMARY KEY,
+  username TEXT UNIQUE NOT NULL,
   avatar TEXT
 );
 
 CREATE TABLE tweets (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   text TEXT NOT NULL,
-  username TEXT REFERENCES github_users(username),
-  email TEXT REFERENCES github_users(email)
+  email TEXT REFERENCES github_users(email),
+  username TEXT REFERENCES github_users(username)
 );
 
 
 INSERT INTO tweets 
-  (text, username, email)
+  (text)
 VALUES
-  ('tweeting is fun', 'Bob-Bob', 'bob@bob.com'),
-  ('tweeting is whatever', 'Sterling', 'sterling@cat.com');
+  ('tweeting is fun'),
+  ('tweeting is whatever');
