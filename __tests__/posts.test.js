cc653cec3708aabfd85b15b256a66e6fc744eab8
Clayton Knapp <csjknapp@gmail.com>
2022-04-06 17:34:04 -0700

    refactores post test to not have authenticate mock and just github utils mock and added agent to tests to use cookies

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -3,22 +3,24 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 const GithubUser = require('../lib/models/GithubUser');
+// const GithubUserService = require('../lib/services/GithubUserService');
 
 jest.mock('../lib/utils/github');
 
-jest.mock('../lib/middleware/authenticate.js', () => (req, res, next) => {
-  req.user = {
-    username: 'bobbob',
-    email: 'bob@bob.com',
-    avatar: 'https://placebear.com/200/300'
-  };
+// jest.mock('../lib/middleware/authenticate.js', () => (req, res, next) => {
+//   req.user = {
+//     username: 'bobbob',
+//     email: 'bob@bob.com',
+//     avatar: 'https://placebear.com/200/300'
+//   };
 
-  next();
-});
+//   next();
+// });
 
 describe('posts tests', () => {
   beforeEach(() => {
     return setup(pool);
+    // .then(() => GithubUserService.create('fake code'));
   });
 
   afterAll(() => {
@@ -26,24 +28,30 @@ describe('posts tests', () => {
   });
 
   it('allow a user to post', async () => {
-    await GithubUser.insert({
-      username: 'bobbob',
-      email: 'bob@bob.com',
-      avatar: 'https://placebear.com/200/300'
-    });
+    // await GithubUser.insert({
+    //   username: 'bobbob',
+    //   email: 'bob@bob.com',
+    //   avatar: 'https://placebear.com/200/300'
+    // });
+
+    //instead of inserting a user
+    // sign in using agent which allows cookies and hits signin endpoint and should use mocked user
+    const agent = request.agent(app);
+    await agent.get('/api/v1/github/login/callback'); //dont need to pass actual code b/c mocked
 
 
     const expected = {
       id: expect.any(String),
-      text: 'bobs tweet',
-      username: 'bobbob',
-      email: 'bob@bob.com',
+      text: 'this is a fake tweet',
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
     };
     
-    const req = await request(app)
+    //changed request(app) to agent to allow cookies
+    const req = await agent
       .post('/api/v1/posts')
       .send({
-        text: 'bobs tweet',
+        text: 'this is a fake tweet',
       });
 
     expect(req.body).toEqual(expected);
@@ -53,13 +61,20 @@ describe('posts tests', () => {
 
   // GET /api/v1/posts lists all posts for all users
   it('gets posts from all users', async () => {
-    await GithubUser.insert({
-      username: 'bobbob',
-      email: 'bob@bob.com',
-      avatar: 'https://placebear.com/200/300'
-    });
+    // await GithubUser.insert({
+    //   username: 'bobbob',
+    //   email: 'bob@bob.com',
+    //   avatar: 'https://placebear.com/200/300'
+    // });
 
-    await request(app)
+
+    //instead of inserting a user
+    // sign in using agent which hits signin endpoint and uses mocked user
+    const agent = request.agent(app);
+    await agent.get('/api/v1/github/login/callback'); //dont need to actually pass a code
+
+    //changed request(app) to agent to allow cookies
+    await agent
       .post('/api/v1/posts')
       .send({
         text: 'tweet 1',
@@ -67,7 +82,8 @@ describe('posts tests', () => {
         // username: 'bobbob'
       });
 
-    await request(app)
+    //changed request(app) to agent to allow cookies
+    await agent
       .post('/api/v1/posts')
       .send({
         text: 'tweet 2',
@@ -79,18 +95,19 @@ describe('posts tests', () => {
       { 
         id: expect.any(String),
         text: 'tweet 1',
-        username: 'bobbob',
-        email: 'bob@bob.com',
+        username: 'fake_github_user',
+        email: 'not-real@example.com',
       },
       { 
         id: expect.any(String),
         text: 'tweet 2',
-        username: 'bobbob',
-        email: 'bob@bob.com',
+        username: 'fake_github_user',
+        email: 'not-real@example.com',
       }
     ];
     
-    const req = await request(app)
+    //changed request(app) to agent to allow cookies
+    const req = await agent
       .get('/api/v1/posts');
 
     expect(req.body).toEqual(expected);
